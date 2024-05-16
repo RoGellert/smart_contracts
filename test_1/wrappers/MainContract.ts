648ce4592a709b52c33a55264826ca1159884172
Roman Gellert <titangtace@gmail.com>
2024-05-16 21:04:11 +0200

    Smarter contract

--- test_1/wrappers/MainContract.ts
+++ test_1/wrappers/MainContract.ts
@@ -6,7 +6,18 @@ import {
     contractAddress, 
     ContractProvider, 
     Sender, 
-    SendMode } from "ton-core";
+    SendMode 
+} from "ton-core";
+
+    
+export type MainContractConfig = {
+    number: number;
+    address: Address;
+};
+
+export function mainContractConfigToCell(config: MainContractConfig): Cell {
+    return beginCell().storeUint(config.number, 32).storeAddress(config.address).endCell();
+  }
 
 export class MainContract implements Contract {
     constructor(
@@ -14,29 +25,34 @@ export class MainContract implements Contract {
         readonly init?: { code: Cell; data: Cell }
     ) {}
 
-    static createFromConfig(config: any, code: Cell, workchain = 0) {
-        const data = beginCell().endCell();
+    static createFromConfig(config: MainContractConfig, code: Cell, workchain = 0) {
+        const data = mainContractConfigToCell(config);
         const init = {code, data};
         const adress = contractAddress(workchain, init);
 
         return new MainContract(adress, init);
     }
 
-    async sendInternalMessage(
+    async sendIncrement(
         provider: ContractProvider,
         sender: Sender,
-        value: bigint
+        value: bigint,
+        increment_by: number
     ) {
+
+      const msg_body = beginCell().storeUint(1, 32).storeUint(increment_by, 32).endCell();
+
       await provider.internal(sender, {
         value,
         sendMode: SendMode.PAY_GAS_SEPARATELY,
-        body: beginCell().endCell(),
+        body: msg_body,
       });
     }
 
     async getData(provider: ContractProvider) {
-        const { stack } = await provider.get("get_the_latest_sender", []);
+        const { stack } = await provider.get("get_contract_storage_data", []);
         return {
+            number: stack.readNumber(),
             recent_sender: stack.readAddress(),
         };
     }
